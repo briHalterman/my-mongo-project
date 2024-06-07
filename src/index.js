1906e2bbb0ef257ab166aaaaff213b29325cbe07
Brittany Halterman <halterma@gmail.com>
2024-06-07 16:39:18 -0400

    Define models and connect to database when starting the application

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 import express from 'express';
 
 import routes from './routes';
+import models, { connectDb } from './models';
 
 const app = express();
 
@@ -21,62 +22,62 @@ app.use('/session', routes.session);
 app.use('/users', routes.user);
 app.use('/messages', routes.message);
 
-let users = {
-  1: {
-    id: '1',
-    username: 'Robin Wieruch'
-  },
-  2: {
-    id: '2',
-    username: 'Dave Davids'
-  }
-};
+// let users = {
+//   1: {
+//     id: '1',
+//     username: 'Robin Wieruch'
+//   },
+//   2: {
+//     id: '2',
+//     username: 'Dave Davids'
+//   }
+// };
 
-let messages = {
-  1: {
-    id: '1',
-    text: 'Hello World',
-    userId: '1'
-  },
-  2: {
-    id: '2',
-    text: 'By World',
-    userId: '2'
-  }
-};
+// let messages = {
+//   1: {
+//     id: '1',
+//     text: 'Hello World',
+//     userId: '1'
+//   },
+//   2: {
+//     id: '2',
+//     text: 'By World',
+//     userId: '2'
+//   }
+// };
 
-app.get('/session', (req, res) => {
-  return res.send(users[req.me.id]);
-});
+// app.get('/session', (req, res) => {
+//   return res.send(users[req.me.id]);
+// });
 
-app.get('/users', (req, res) => {
-  return res.send(Object.values(users));
-});
+// app.get('/users', (req, res) => {
+//   return res.send(Object.values(users));
+// });
 
-app.get('/users/:userId', (req, res) => {
-  return res.send(users[req.params.userId]);
-});
+// app.get('/users/:userId', (req, res) => {
+//   return res.send(users[req.params.userId]);
+// });
 
-app.get('/messages', (req, res) => {
-  return res.send(Object.values(messages));
-});
+// app.get('/messages', (req, res) => {
+//   return res.send(Object.values(messages));
+// });
 
-app.get('/messages/:messageId', (req, res) => {
-  return res.send(messages[req.params.messageId]);
-});
+// app.get('/messages/:messageId', (req, res) => {
+//   return res.send(messages[req.params.messageId]);
+// });
 
-app.post('/messages', (req, res) => {
-  const id = uuidv4();
-  const message = {
-    id,
-    text: req.body.text,
-    userId: req.me.id
-  };
+// app.post('/messages', (req, res) => {
+//   const id = uuidv4();
+//   const message = {
+//     id,
+//     text: req.body.text,
+//     userId: req.me.id
+//   };
 
-  messages[id] = message;
+//   messages[id] = message;
 
-  return res.send(message);
-});
+//   return res.send(message);
+// });
 
 // app.put('/users/:userId', (req, res) => {
 //   return res.send(
@@ -84,17 +85,28 @@ app.post('/messages', (req, res) => {
 //   );
 // });
 
-app.delete('/messages/:messageId', (req, res) => {
-  const {
-    [req.params.messageId]: message,
-    ...otherMessages
-  } = messages;
+// app.delete('/messages/:messageId', (req, res) => {
+//   const {
+//     [req.params.messageId]: message,
+//     ...otherMessages
+//   } = messages;
 
-  messages = otherMessages;
+//   messages = otherMessages;
 
-  return res.send(message);
+//   return res.send(message);
+// });
+
+const eraseDatabaseOnSync = true;
+
+connectDb().then(async () => {
+  if (eraseDatabaseOnSync) {
+    await Promise.all([
+      models.User.deleteMany({}),
+      models.Message.deleteMany({})
+    ]);
+  }
+
+  app.listen(process.env.PORT, () =>
+    console.log(`Example app listening on port ${process.env.PORT}!`)
+  );
 });
-
-app.listen(process.env.PORT, () =>
-  console.log(`Example app listening on port ${process.env.PORT}!`)
-);

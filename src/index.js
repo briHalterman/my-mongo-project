a48316b4028021bfe040e1ea4c1b0d9f9305cc23
Brittany Halterman <halterma@gmail.com>
2024-06-09 12:46:35 -0400

    Remove dead code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import 'dotenv/config';
-import { v4 as uuidv4 } from 'uuid';
 import cors from 'cors';
 import express from 'express';
 
@@ -8,11 +7,14 @@ import models, { connectDb } from './models';
 
 const app = express();
 
+// Third-Party Middleware
 app.use(cors());
 
+// Built-In Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Custom Middleware
 app.use(async (req, res, next) => {
   req.context = {
     models,
@@ -22,84 +24,12 @@ app.use(async (req, res, next) => {
   next();
 });
 
+// Routes
 app.use('/session', routes.session);
 app.use('/users', routes.user);
 app.use('/messages', routes.message);
 
-// let users = {
-//   1: {
-//     id: '1',
-//     username: 'Robin Wieruch'
-//   },
-//   2: {
-//     id: '2',
-//     username: 'Dave Davids'
-//   }
-// };
-
-// let messages = {
-//   1: {
-//     id: '1',
-//     text: 'Hello World',
-//     userId: '1'
-//   },
-//   2: {
-//     id: '2',
-//     text: 'By World',
-//     userId: '2'
-//   }
-// };
-
-// app.get('/session', (req, res) => {
-//   return res.send(users[req.me.id]);
-// });
-
-// app.get('/users', (req, res) => {
-//   return res.send(Object.values(users));
-// });
-
-// app.get('/users/:userId', (req, res) => {
-//   return res.send(users[req.params.userId]);
-// });
-
-// app.get('/messages', (req, res) => {
-//   return res.send(Object.values(messages));
-// });
-
-// app.get('/messages/:messageId', (req, res) => {
-//   return res.send(messages[req.params.messageId]);
-// });
-
-// app.post('/messages', (req, res) => {
-//   const id = uuidv4();
-//   const message = {
-//     id,
-//     text: req.body.text,
-//     userId: req.me.id
-//   };
-
-//   messages[id] = message;
-
-//   return res.send(message);
-// });
-
-// app.put('/users/:userId', (req, res) => {
-//   return res.send(
-//     `PUT HTTP method on user/${req.params.userId} resource`
-//   );
-// });
-
-// app.delete('/messages/:messageId', (req, res) => {
-//   const {
-//     [req.params.messageId]: message,
-//     ...otherMessages
-//   } = messages;
-
-//   messages = otherMessages;
-
-//   return res.send(message);
-// });
-
+// Start
 const eraseDatabaseOnSync = true;
 
 connectDb().then(async () => {
